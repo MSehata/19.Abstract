a18de91fad344711b4a8093b0eaace61b744b9bc
Moloko Sehata <moloko4@live.com>
2023-10-10 13:40:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Abstract
+{
+    abstract class vehicle
+    {
+        public int wheels = 4;
+    }
+
+    class Corsa : vehicle 
+    {
+        public int spareWheets = 1;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Corsa car2 = new Corsa();
+            Console.WriteLine("Car 2 wheels: " + car2.wheels);
+
+            Console.Read();
+
+        }
+    }
+}
